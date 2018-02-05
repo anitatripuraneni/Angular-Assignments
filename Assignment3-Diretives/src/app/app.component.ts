9689652f07616a1438b7fd547d89312ee1079d24
anitatripuraneni <anita.tripuraneni@gmail.com>
2018-02-05 16:15:08 +0530

    commit the changes

--- Assignment3-Diretives/src/app/app.component.ts
+++ Assignment3-Diretives/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['' +
+  '.online{' +
+  'color: white;' +
+  '}'
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  paragraph = 'sceret password is displayed here';
+  displayContent1 = false;
+  count = 0;
+  countArr = [];
+
+  displayContent() {
+    this.displayContent1 = true;
+    this.count++;
+    this.countArr.push(this.count);
+  }
+
+  getColor() {
+    return this.count > 5 ? 'green' : 'white';
+  }
 }
